8c21dde711ea2937349ae99ab69d92ee45593694
NikSakh <NikSakharow@gmail.com>
2025-04-04 15:57:50 +0300

    Добавлена структура проекта с HelloWorld и HelloGit

---
+++ src/main/java/ru/mentee/power/HelloGit.java
@@ -0,0 +1,9 @@
+package ru.mentee.power;
+&nbsp;
+
+public class HelloGit{
+    public static void main(String[] args){
+        System.out.println("Привет, Git и GitHub!");
+        System.out.println("Я начинаю изучение Java!");
+    }
+}
---
+++ src/main/java/ru/mentee/power/HelloWorld.java
@@ -0,0 +1,8 @@
+package ru.mentee.power;
+&nbsp;
+
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Привет, мир!");
+    }
+}
